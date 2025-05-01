7b1372cac2a226d36c2e15175c5bf6424254bb20
abhinay <abhinay212@gmail.com>
2025-05-01 17:43:05 +0530

    Refactor: Implement filter, map, onEach, and count operators on countDownFlow
    The `countDownFlow` now utilizes the `filter`, `map`, `onEach`, and `count` operators to transform and count the emitted values. Specifically, it filters for even numbers, maps them to their squares, prints each processed value and then counts the number of even values. The original collecting of values has been commented out.

--- app/src/main/java/com/abhinay/kotlinflows/MainViewModel.kt
+++ app/src/main/java/com/abhinay/kotlinflows/MainViewModel.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 
@@ -34,10 +38,28 @@ class MainViewModel : ViewModel() {
 
     private fun collectFlow() {
         viewModelScope.launch {
-            countDownFlow.collect { time ->
-                delay(1500L)
+         val count =  countDownFlow
+                .filter { time ->
+                    time % 2 == 0
+                }
+                .map { time ->
+                    time * time
+                }
+                .onEach { time ->
+                    println(time)
+                }
+                .count {
+                    it % 2 == 0
+                }
+            println("The Count is $count")
+
+
+
+
+               /* .collect { time ->
+                *//*delay(1500L)*//*
                 println("The current time is $time")
-            }
+            }*/
         }
 
     }
