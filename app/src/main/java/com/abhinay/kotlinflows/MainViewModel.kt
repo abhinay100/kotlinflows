a0ca79da69130f61477022587a69d4941fccdf19
abhinay <abhinay212@gmail.com>
2025-05-01 17:13:01 +0530

    Initial commit

---
+++ app/src/main/java/com/abhinay/kotlinflows/MainViewModel.kt
@@ -0,0 +1,45 @@
+package com.abhinay.kotlinflows
+
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+
+/**
+ * Created by Abhinay on 29/04/25.
+ *
+ *
+ */
+class MainViewModel : ViewModel() {
+
+    val countDownFlow = flow<Int> {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 0) {
+            delay(1000L)
+            currentValue--
+            emit(currentValue)
+        }
+
+    }
+
+    init {
+        collectFlow()
+    }
+
+    private fun collectFlow() {
+        viewModelScope.launch {
+            countDownFlow.collect { time ->
+                delay(1500L)
+                println("The current time is $time")
+            }
+        }
+
+    }
+
+}
